818c571b2beb91c08f87b9d769f8eaeaf03231cd
Severin Nandillon <nandillons@gmail.com>
2020-12-31 01:14:05 +0100

    problème url fixé, fausse manip pendant le merge

--- FlickrPhotoSearchAPI/src/app/flikr-api.service.ts
+++ FlickrPhotoSearchAPI/src/app/flikr-api.service.ts
@@ -10,11 +10,11 @@ export class FlikrAPIService {
   constructor(private http: HttpClient) { }
 
   // tslint:disable-next-line:variable-name
-  api_key = '5e6cf683ffcfca04200286e429c5b2de';
+  api_key = 'e5dd26b88b7becce013b470f444d1a2c';
 
   getFlikrImg(tag): Observable<any>{
     // tslint:disable-next-line:max-line-length
-    return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.api_key + '&tags=' + tag + '&format=json&nojsoncallback=1');
+    return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.api_key + '&' + tag + '&format=json&nojsoncallback=1');
   }
   // tslint:disable-next-line:variable-name
   getFlikrInfo(photo_id): Observable<any>{
