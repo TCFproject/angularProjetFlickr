abdd2ed11b1d10914256439826cb994453a5aa7b
Severin Nandillon <nandillons@gmail.com>
2021-01-22 16:28:27 +0100

    récup master

--- FlickrPhotoSearchAPI/src/app/button-field/button-field.component.ts
+++ FlickrPhotoSearchAPI/src/app/button-field/button-field.component.ts
@@ -15,44 +15,35 @@ export class ButtonFieldComponent implements OnInit {
     donnee: any[];
     passage = 0;
     ngOnInit(): void {
-        this.imgApi.getFlikrImg('nature').subscribe(
+        this.imgApi.getFlikrImg(this.Tags).subscribe(
             data => {
                 this.donnee = data.photos.photo;
             }
         );
     }
   test(): { lien: string; Id: number }[] {return [
-    // tslint:disable-next-line:max-line-length
     {lien: 'https://live.staticflickr.com/' + this.Tags[this.passage].server + '/' + this.Tags[this.passage].id + '_' + this.Tags[this.passage].secret + '.jpg', Id: this.passage},
-    // tslint:disable-next-line:max-line-length
     {lien: 'https://live.staticflickr.com/' + this.Tags[this.passage + 1].server + '/' + this.Tags[this.passage + 1].id + '_' + this.Tags[this.passage + 1].secret + '.jpg', Id: this.passage + 1},
-    // tslint:disable-next-line:max-line-length
     {lien: 'https://live.staticflickr.com/' + this.Tags[this.passage + 2].server + '/' + this.Tags[this.passage + 2].id + '_' + this.Tags[this.passage + 2].secret + '.jpg', Id: this.passage + 2},
-    // tslint:disable-next-line:max-line-length
     {lien: 'https://live.staticflickr.com/' + this.Tags[this.passage + 3].server + '/' + this.Tags[this.passage + 3].id + '_' + this.Tags[this.passage + 3].secret + '.jpg', Id: this.passage + 3},
-    // tslint:disable-next-line:max-line-length
     {lien: 'https://live.staticflickr.com/' + this.Tags[this.passage + 4].server + '/' + this.Tags[this.passage + 4].id + '_' + this.Tags[this.passage + 4].secret + '.jpg', Id: this.passage + 4} ]; }
 
-  // tslint:disable-next-line:typedef
   Dropup() {
-    // @ts-ignore
     if (this.passage >= this.donnee.length - 5){
       this.passage = this.donnee.length - 5;
     }else {
-      this.passage++;
+      this.passage += 5;
     }
   }
 
-  // tslint:disable-next-line:typedef
   Dropdown() {
-    // @ts-ignore
     if (this.passage === 0){
       this.passage = 0;
     }else {
-      this.passage--;
+      this.passage -= 5;
     }
   }
-  // tslint:disable-next-line:typedef
+
   ouvrirInfo(info) {
       const dial = this.dialog.open(InfoImageDialogComponent, {data: {infos: info}});
   }
