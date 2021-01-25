20054d8084d034ea6d44954e955efa2541e08b9a
Severin Nandillon <nandillons@gmail.com>
2021-01-25 14:26:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require("./FlickrPhotoSearchAPI/node_modules/express");
 var mongo = require("./FlickrPhotoSearchAPI/node_modules/mongoose");
 const LOCAL_DB = "mongodb://localhost:27017/flickr";
 
-var db = mongo.connect(LOCAL_DB, (err, res) => {
+var db = mongo.connect(LOCAL_DB, (err) => {
   if (err) { console.log(err); }
-  else { console.log('Connected to local db.'); }
+  else { console.log('Connecté à mongodb localement.'); }
 });
 
 var app = express();
-app.use((req, res, next) => {
+app.use((res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   next();
 });
