9beba3e9e7d89eed0b91b1d92d81f3292500ef7f
Severin Nandillon <nandillons@gmail.com>
2021-01-26 12:54:08 +0100

    fix dropup error

--- FlickrPhotoSearchAPI/src/app/flikr-api.service.ts
+++ FlickrPhotoSearchAPI/src/app/flikr-api.service.ts
@@ -9,11 +9,12 @@ export class FlikrAPIService {
 
   constructor(private http: HttpClient) { }
 
-  api_key = '26e0de555f194b85c18c29ff7277f3a1';
+  api_key = '4b1eeca90d00c40090f976710f0e125b';
 
   getFlikrImg(tag): Observable<any> {
     return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.api_key + '&tags=' + tag + '&format=json&nojsoncallback=1');
   }
+
   getFlikrInfo(photo_id): Observable<any> {
     return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=' + this.api_key + '&photo_id=' + photo_id + '&format=json&nojsoncallback=1');
   }
