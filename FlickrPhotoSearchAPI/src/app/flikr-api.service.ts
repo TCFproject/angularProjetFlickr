e3d2e6b890aa0ee20e5d8f2b06ca7e18fb4466fc
Severin Nandillon <nandillons@gmail.com>
2021-01-26 13:01:33 +0100

    Update flikr-api.service.ts

--- FlickrPhotoSearchAPI/src/app/flikr-api.service.ts
+++ FlickrPhotoSearchAPI/src/app/flikr-api.service.ts
@@ -15,14 +15,14 @@ export class FlikrAPIService {
   httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
 
   getFlikrImg(tag): Observable<any> {
-    return this.http.post(this.NODEJS, { 
+    this.http.post(this.NODEJS, { 
       "tag": tag,
       "url": "test_angular",
       "auteur": "test_angular",
       "titre": "test_angular",
       "datepost": "test_angular"
     }, {headers: this.httpHeaders});
-    // return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.api_key + '&tags=' + tag + '&format=json&nojsoncallback=1');
+    return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.api_key + '&tags=' + tag + '&format=json&nojsoncallback=1');
   }
   getFlikrInfo(photo_id): Observable<any> {
     return this.http.get('https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=' + this.api_key + '&photo_id=' + photo_id + '&format=json&nojsoncallback=1');
