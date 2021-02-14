c2afb78afc1000205f268b3f2c0d9a75c1af1977
Severin Nandillon <nandillons@gmail.com>
2021-02-14 15:21:23 +0100

    Vérification de l'input -> 1 seul tag possible, sinon on prend seulement le premier

--- FlickrPhotoSearchAPI/src/app/flickr-api.service.ts
+++ FlickrPhotoSearchAPI/src/app/flickr-api.service.ts
@@ -13,12 +13,21 @@ export class FlickrAPIService {
 
   constructor(private http: HttpClient) { }
 
-  api_key: string = '39293cea7646ac5004421a4acadfa1fa';
+  api_key: string = '7d3de17b9c43cd99c2c7372650bb502e';
   NODEJS: string = "http://localhost:7000/images";
   httpHeaders: HttpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
 
   getFlickrImg(tag: string): Observable<any> {
     let apiUrl = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.api_key + '&tags=' + tag + '&format=json&nojsoncallback=1';
+    /**
+     * pour permettre la recherche avec plusieurs tags, il faut remplacer les virgules par leur code ASCII dans l'url, %2C 
+     * mais trop compliqué de mettre en base les recherches qui ont plusieurs tags 
+     * tout en différenciant les recherches qui ne contiennent qu'un seul tag étant donné 
+     * qu'on peut ne pas avoir les mêmes photos en recherchant avec un seul tag et  
+     * en recherchant avec plusieurs tags 
+     */
+    // let tagFormat = tag.split(/[ ,]+/).join('%2C');
+    tag = tag.match(/[a-zA-Z0-9]+/)[0];
     return this.http.get(this.NODEJS + "/" + tag.toLowerCase()).pipe(
       mergeMap(
         (result) => {
